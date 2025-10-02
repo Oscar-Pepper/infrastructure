ff4689183074fcee877b9a09f87b9c942149e878
Oscar Pepper <oscarpepper@zingolabs.org>
2025-10-02 15:35:44 +0100

    add quotations to fix config

--- zcash_local_net/src/config.rs
+++ zcash_local_net/src/config.rs
@@ -237,7 +237,7 @@ NU6 = {nu6_activation_height}"
             config_file.write_all(
                 format!(
                     "\n
-NU6.1 = {nu6_1_activation_height}"
+\"NU6.1\" = {nu6_1_activation_height}"
                 )
                 .as_bytes(),
             )?;
